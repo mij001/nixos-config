790f49e4d909179256965c70522698489851b8d7
Inomal <no@tell.lol>
2024-08-01 10:30:32 +0530

    initial commit for nix-config v2

---
+++ configuration.nix
@@ -0,0 +1,236 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+    ];
+
+  # Bootloader.
+  # boot.loader.systemd-boot.enable = true;
+
+  boot.loader = {
+    efi = {
+      canTouchEfiVariables = true;
+      # assuming /boot is the mount point of the  EFI partition in NixOS (as the installation section recommends).
+      efiSysMountPoint = "/boot";
+    };
+    grub = {
+      # despite what the configuration.nix manpage seems to indicate,
+      # as of release 17.09, setting device to "nodev" will still call
+      # `grub-install` if efiSupport is true
+      # (the devices list is not used by the EFI grub install,
+      # but must be set to some value in order to pass an assert in grub.nix)
+      devices = [ "nodev" ];
+      efiSupport = true;
+      enable = true;
+    };
+  };
+
+
+  networking.hostName = "nixos"; # Define your hostname.
+  #networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Enable networking
+  networking.networkmanager.enable = true;
+  networking.networkmanager.dns = "dnsmasq";
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+
+  # Set your time zone.
+  time.timeZone = "Asia/Colombo";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  # Enable the X11 windowing system.
+  # You can disable this if you're only using the Wayland session.
+  services.xserver.enable = true;
+
+  # Enable the KDE Plasma Desktop Environment.
+# services.displayManager.sddm.wayland.enable = true;
+  services.displayManager.sddm.enable = true;
+  services.desktopManager.plasma6.enable = true;
+
+  ######################### Heperland setup #######################
+/*
+  programs.hyprland = { # we use this instead of putting it in systemPackages/users
+    enable = true;
+    xwayland.enable = true;
+  };
+
+  environment.sessionVariables.NIXOS_OZONE_WL = "1"; # This variable fixes electron apps in wayland
+
+
+
+  # NVIDIA drivers are unfree.
+  services.xserver.videoDrivers = [ "nvidia" "intel"]; # If you are using a hybrid laptop add its iGPU manufacturer
+  hardware.opengl = {
+    enable = true;
+    driSupport = true;
+    driSupport32Bit = true;
+  };
+
+  hardware.nvidia = {
+    # Enable modesetting for Wayland compositors (hyprland)
+    modesetting.enable = true;
+    # Use the open source version of the kernel module (for driver 515.43.04+)
+    open = true;
+    # Enable the Nvidia settings menu
+    nvidiaSettings = true;
+    # Select the appropriate driver version for your specific GPU
+    package = config.boot.kernelPackages.nvidiaPackages.stable;
+  };*/
+  #################################################################
+
+  # Configure keymap in X11
+#    services.xserver = {
+#      layout = "us";
+#      xkbVariant = "";
+#    };
+
+  # Enable CUPS to print documents.
+  services.printing.enable = true;
+
+  # Enable sound with pipewire.
+  hardware.pulseaudio.enable = false;
+
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    audio.enable = true;
+    pulse.enable = true;
+    alsa = {
+      enable = true;
+      support32Bit = true;
+    };
+    wireplumber.enable = true;
+    #jack.enable = true;
+
+    # use the example session manager (no others are packaged yet so this is enabled by default,
+    # no need to redefine it in your config for now)
+    #media-session.enable = true;
+  };
+
+  # Enable touchpad support (enabled default in most desktopManager).
+  # services.xserver.libinput.enable = true;
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.defaultUserShell = pkgs.zsh;
+  programs.zsh.enable = true;
+
+  users.users.inomal = {
+    isNormalUser = true;
+    description = "Mayura Inomal";
+    extraGroups = [ "networkmanager" "wheel" "docker" "libvirtd" "audio"];
+    packages = with pkgs; [
+      kdePackages.kate
+    #  thunderbird
+    ];
+  };
+
+  virtualisation.libvirtd.enable = true;
+
+  # To install docker, add the following to your your NixOS configuration:
+  virtualisation.docker.enable = true;
+  virtualisation.docker.daemon.settings = {
+  data-root = "/home/inomal/docker";
+  };
+
+  # To make sure some docker containers are running as systemd services, you can use 'oci-containers':
+
+#   virtualisation.oci-containers = {
+#     backend = "docker";
+#     containers = {
+#       foo = {
+#         # ...
+#       };
+#     };
+#   };
+
+  environment.systemPackages = with pkgs; [
+  #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+
+     # Flakes clones its dependencies through the git command,
+     # so git must be installed first
+     git
+
+     wget
+
+
+
+  ];
+
+  ########### Needed fonts
+  fonts.packages = with pkgs; [
+  fira-code
+  fira-code-symbols
+  mplus-outline-fonts.githubRelease
+  dina-font
+  proggyfonts
+  wineWowPackages.fonts
+  corefonts
+  vistafonts
+];
+
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
+
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  # services.openssh.enable = true;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+
+
+
+
+
+
+
+
+
+
+   # Enable the Flakes feature and the accompanying new nix command-line tool
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
+  # Set the default editor to vim
+  environment.variables.EDITOR = "vim";
+  environment.sessionVariables = {
+  XDG_DATA_HOME = "$HOME/.local/share";
+  XDG_CONFIG_HOME = "$HOME/.local/config";
+  XDG_CACHE_HOME = "$HOME/.local/cache";
+
+  NIXOS_OZONE_WL = "1";
+};
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "24.05"; # Did you read the comment?
+
+}
---
+++ flake.nix
@@ -0,0 +1,41 @@
+
+{
+  description = "Inomal NixOS flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
+    # home-manager, used for managing user configuration
+    home-manager = {
+      url = "github:nix-community/home-manager/release-24.05";
+      # The `follows` keyword in inputs is used for inheritance.
+      # Here, `inputs.nixpkgs` of home-manager is kept consistent with
+      # the `inputs.nixpkgs` of the current flake,
+      # to avoid problems caused by different versions of nixpkgs.
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = inputs@{ nixpkgs, home-manager, ... }: {
+    nixosConfigurations = {
+
+        nixos = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          ./configuration.nix
+
+          # make home-manager as a module of nixos
+          # so that home-manager configuration will be deployed automatically when executing `nixos-rebuild switch`
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+
+            home-manager.users.inomal = import ./home.nix;
+
+            # Optionally, use home-manager.extraSpecialArgs to pass arguments to home.nix
+          }
+        ];
+      };
+    };
+  };
+}
